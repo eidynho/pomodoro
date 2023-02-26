75b5e2dad26a96232af78349387e3019ff9db7f7
eidy <zviniciuseidy2@gmail.com>
2023-02-26 16:25:02 -0300

    new: status column in history table

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -48,6 +48,7 @@ export const HistoryList = styled.div`
             padding: 1rem;
             font-size: 0.875rem;
             line-height: 1.6;
+            min-width: 7rem;
 
             &:first-child {
                 width: 50%;
@@ -58,5 +59,29 @@ export const HistoryList = styled.div`
                 padding-right: 1.5rem;
             }
         }
+    }
+`;
+
+const STATUS_COLORS = {
+    yellow: "yellow-500",
+    green: "green-500",
+    red: "red-500",
+} as const;
+
+interface StatusProps {
+    statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span<StatusProps>`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &::before {
+        content: "";
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 9999px;
+        background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
     }
 `;
